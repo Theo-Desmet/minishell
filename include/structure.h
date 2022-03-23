d1a1893cb0a8fa39d124f00a2612004c57b8d5c7
Theo DESMET <tdesmet@e2r3p19.clusters.42paris.fr>
2022-03-23 12:09:36 +0100

    add ast structur

--- include/structure.h
+++ include/structure.h
@@ -6,27 +6,31 @@
 /*   By: tdesmet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 10:35:26 by tdesmet           #+#    #+#             */
-/*   Updated: 2022/03/23 11:21:14 by tdesmet          ###   ########.fr       */
+/*   Updated: 2022/03/23 12:08:17 by tdesmet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCTURE_H
 # define STRUCTURE_H
 
-global int SIGRET;
+int SIGRET;
 
 typedef struct s_list{
-	int	val;
-	int	before;
-	int	next;
+	char	*str;
+	int	type;
+	struct s_list	*before;
+	struct s_list	*next;
 }	t_list;
 
-typedef struct s_data{
-	t_parsing	parsing;
-	t_arg		arg;
-	t_cmd		cmd;
-	char *actual_path;
-}	t_data;
+typedef struct s_ast_node{
+	struct s_ast_info	*info;
+	struct s_ast_node	*left;
+	struct s_ast_node	*right;
+}	t_ast_node;
+
+typedef struct s_ast_info{
+
+}	t_ast_info;
 
 typedef struct s_parsing{
 	int	nb_args;
@@ -43,4 +47,11 @@ typedef struct s_cmd{
 	char *cmd;
 	char *path;
 }	t_cmd;
+
+typedef struct s_data{
+	t_parsing	parsing;
+	t_arg		arg;
+	t_cmd		cmd;
+	char *actual_path;
+}	t_data;
 #endif
